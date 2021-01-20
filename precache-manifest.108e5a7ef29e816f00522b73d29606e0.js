063ee0675f1f862a45164f37589247c98dd35e18
Igor Esposito <sposigor@gmail.com>
2021-01-20 12:32:08 -0300

    Updates

---
+++ precache-manifest.108e5a7ef29e816f00522b73d29606e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2a3447726698b471b35a93e9288f6d",
+    "url": "/spos.github.io/index.html"
+  },
+  {
+    "revision": "eefb40a0842ca1612127",
+    "url": "/spos.github.io/static/css/main.a720a78b.chunk.css"
+  },
+  {
+    "revision": "c008be051e6a7b593f38",
+    "url": "/spos.github.io/static/js/2.6c2fc52a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/spos.github.io/static/js/2.6c2fc52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eefb40a0842ca1612127",
+    "url": "/spos.github.io/static/js/main.136ba411.chunk.js"
+  },
+  {
+    "revision": "b80d11ca48ae20dd209b",
+    "url": "/spos.github.io/static/js/runtime-main.b5b23d0b.js"
+  }
+]);
